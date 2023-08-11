32e269a4393ce642c020706c0e7454b59d56aee4
Kooreis <kooreis.official@gmail.com>
2023-08-10 21:52:50 -0500

    Imported necessary modules: json for parsing and formatting JSON strings, and sys for accessing command-line arguments.

--- Text_Based_JSON_Beautifier.py
+++ Text_Based_JSON_Beautifier.py
@@ -1,19 +1,2 @@
-```python
 import json
 import sys
-
-def beautify_json(json_string):
-    parsed_json = json.loads(json_string)
-    return json.dumps(parsed_json, indent=4, sort_keys=True)
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <json_string>")
-        sys.exit(1)
-
-    json_string = sys.argv[1]
-    print(beautify_json(json_string))
-
-if __name__ == "__main__":
-    main()
-```
