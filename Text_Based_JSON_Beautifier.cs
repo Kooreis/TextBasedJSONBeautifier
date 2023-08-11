acf362da82660e8ecfa0f3748cabd51f943aee70
Kooreis <kooreis.official@gmail.com>
2023-08-10 21:52:50 -0500

    Added necessary namespaces for the program. System for basic C# functionalities and Newtonsoft.Json for JSON parsing.

--- Text_Based_JSON_Beautifier.cs
+++ Text_Based_JSON_Beautifier.cs
@@ -1,24 +1,2 @@
-```CSharp
 using System;
 using Newtonsoft.Json;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Enter JSON text:");
-        string jsonInput = Console.ReadLine();
-        try
-        {
-            var parsedJson = JsonConvert.DeserializeObject(jsonInput);
-            string beautifiedJson = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
-            Console.WriteLine("Beautified JSON:");
-            Console.WriteLine(beautifiedJson);
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Invalid JSON");
-        }
-    }
-}
-```
